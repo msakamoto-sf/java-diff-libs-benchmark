4c9e3a1e2dc5342bd7925624ea7a56ba34de1b94
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2018-01-14 16:33:58 +0900

    add files-diff benchmark

--- src/main/java/net/glamenvseptzen/javadifflibsbenchmark/BenchamrkDiffFamily.java
+++ src/main/java/net/glamenvseptzen/javadifflibsbenchmark/BenchamrkDiffFamily.java
@@ -1,9 +1,16 @@
 package net.glamenvseptzen.javadifflibsbenchmark;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import com.cloudbees.diff.Diff;
 import com.sksamuel.diffpatch.DiffMatchPatch;
@@ -12,6 +19,26 @@ import net.glamenvseptzen.javadifflibsbenchmark.BenchmarkResult.Tick;
 
 public class BenchamrkDiffFamily {
     private final Map<String, Map<String, BenchmarkResult>> brdata = new LinkedHashMap<>();
+    private final File filesDiffBaseDir;
+    private final File filesDiffCmpDir;
+    private final boolean enableFilesDiff;
+    private final String filesDiffBaseDirAbs;
+    private final String filesDiffCmpDirAbs;
+
+    public BenchamrkDiffFamily(final File filesDiffBaseDir, final File filesDiffCmpDir) {
+        this.filesDiffBaseDir = filesDiffBaseDir;
+        this.filesDiffCmpDir = filesDiffCmpDir;
+        if (Objects.nonNull(this.filesDiffBaseDir) && Objects.nonNull(this.filesDiffCmpDir)
+                && this.filesDiffBaseDir.isDirectory() && this.filesDiffCmpDir.isDirectory()) {
+            enableFilesDiff = true;
+            filesDiffBaseDirAbs = this.filesDiffBaseDir.getAbsolutePath();
+            filesDiffCmpDirAbs = this.filesDiffCmpDir.getAbsolutePath();
+        } else {
+            enableFilesDiff = false;
+            filesDiffBaseDirAbs = null;
+            filesDiffCmpDirAbs = null;
+        }
+    }
 
     private BenchmarkResult getBenchmarkResult(final String patternName, final String algName) {
         Map<String, BenchmarkResult> algToBr = brdata.getOrDefault(patternName, new LinkedHashMap<>());
@@ -50,6 +77,70 @@ public class BenchamrkDiffFamily {
         dfp.Diff_Timeout = 0.0f; // set no-timeout
         dfp.diff_main(p.base, p.cmp);
         tick1.tack();
+    }
+
+    class FilesDiffPair {
+        final File base;
+        final File cmp;
+
+        FilesDiffPair(final File base, final File cmp) {
+            this.base = base;
+            this.cmp = cmp;
+        }
+    }
+
+    private List<FilesDiffPair> filesDiffPairs = new ArrayList<>();
+
+    public void traverse(File dir) {
+        for (File f : dir.listFiles()) {
+            if (f.isDirectory()) {
+                traverse(f);
+                continue;
+            }
+            final String absBasePath = f.getAbsolutePath();
+            final String absCmpPath = absBasePath.replace(filesDiffBaseDirAbs, filesDiffCmpDirAbs);
+            final File cmpFile = new File(absCmpPath);
+            if (!cmpFile.exists()) {
+                continue;
+            }
+            filesDiffPairs.add(new FilesDiffPair(f, cmpFile));
+        }
+    }
+
+    public void benchmarkFiles() throws IOException {
+        if (!enableFilesDiff) {
+            return;
+        }
+        traverse(this.filesDiffBaseDir);
+        for (FilesDiffPair pair : filesDiffPairs) {
+            final Charset cs = StandardCharsets.ISO_8859_1;
+            final String baseStr = new String(Files.readAllBytes(pair.base.toPath()), cs);
+            final String cmpStr = new String(Files.readAllBytes(pair.cmp.toPath()), cs);
+            List<String> baseByLine = Files.readAllLines(pair.base.toPath(), cs);
+            List<String> cmpByLine = Files.readAllLines(pair.cmp.toPath(), cs);
+            final String patternName = "files-diff";
+
+            IBenchmark bm = new BenchmarkJavaDiffUtilsByLine();
+            bm.diffByLine(getBenchmarkResult(patternName, "java-diff-utils:myers:by-line"), baseByLine, cmpByLine);
+
+            bm = new BenchmarkJGitMyers();
+            bm.diffByLine(getBenchmarkResult(patternName, "jgit:myers:by-line"), baseByLine, cmpByLine);
+
+            bm = new BenchmarkJGitHistogram();
+            bm.diffByLine(getBenchmarkResult(patternName, "jgit:histogram:by-line"), baseByLine, cmpByLine);
+
+            BenchmarkResult br = getBenchmarkResult(patternName, "diff4j:HuntDiff");
+            Tick tick0 = br.tick();
+            Diff.diff(baseByLine, cmpByLine, false);
+            tick0.tack();
+
+            br = getBenchmarkResult(patternName, "google-diff-match-patch");
+            Tick tick1 = br.tick();
+            DiffMatchPatch dfp = new DiffMatchPatch();
+            dfp.Diff_Timeout = 0.0f; // set no-timeout
+            dfp.diff_main(baseStr, cmpStr);
+            tick1.tack();
+        }
     }
 
     public void printResults() {
--- src/main/java/net/glamenvseptzen/javadifflibsbenchmark/Main.java
+++ src/main/java/net/glamenvseptzen/javadifflibsbenchmark/Main.java
@@ -1,5 +1,7 @@
 package net.glamenvseptzen.javadifflibsbenchmark;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -12,15 +14,14 @@ public class Main {
         }
     }
 
-    static final int LOOP_PER_PATTERNS = 5;
-
-    public static void doBenchmark() {
+    public static void doBenchmark(final int patternLoopCount, final File filesDiffBaseDir,
+            final File filesDiffCmpDir) {
         SecureRandom sr = new SecureRandom();
         Random rand = new Random(sr.nextLong());
-        BenchamrkDiffFamily bdf = new BenchamrkDiffFamily();
+        BenchamrkDiffFamily bdf = new BenchamrkDiffFamily(filesDiffBaseDir, filesDiffCmpDir);
 
         System.out.println("p1");
-        for (int i = 0; i < LOOP_PER_PATTERNS; i++) {
+        for (int i = 0; i < patternLoopCount; i++) {
             RandomStringPair p = RandomStringPair.nextEmptyRandomPair(rand);
             bdf.benchmark("base:empty <> cmp:random", p);
             System.out.print(".");
@@ -28,7 +29,7 @@ public class Main {
         System.out.println("");
 
         System.out.println("p2");
-        for (int i = 0; i < LOOP_PER_PATTERNS; i++) {
+        for (int i = 0; i < patternLoopCount; i++) {
             RandomStringPair p = RandomStringPair.nextRandomEmptyPair(rand);
             bdf.benchmark("base:random <> cmp:empty", p);
             System.out.print(".");
@@ -36,7 +37,7 @@ public class Main {
         System.out.println("");
 
         System.out.println("p3");
-        for (int i = 0; i < LOOP_PER_PATTERNS; i++) {
+        for (int i = 0; i < patternLoopCount; i++) {
             RandomStringPair p = RandomStringPair.nextRandomPair(rand);
             bdf.benchmark("base:random <> cmp:random", p);
             System.out.print(".");
@@ -44,7 +45,7 @@ public class Main {
         System.out.println("");
 
         System.out.println("p4");
-        for (int i = 0; i < LOOP_PER_PATTERNS; i++) {
+        for (int i = 0; i < patternLoopCount; i++) {
             RandomStringPair p = RandomStringPair.nextPrefixedRandomPair(rand);
             bdf.benchmark("base:random <> cmp:(base+)random", p);
             System.out.print(".");
@@ -52,7 +53,7 @@ public class Main {
         System.out.println("");
 
         System.out.println("p5");
-        for (int i = 0; i < LOOP_PER_PATTERNS; i++) {
+        for (int i = 0; i < patternLoopCount; i++) {
             RandomStringPair p = RandomStringPair.nextRandomSuffixedPair(rand);
             bdf.benchmark("base:random <> cmp:random(+base)", p);
             System.out.print(".");
@@ -60,22 +61,44 @@ public class Main {
         System.out.println("");
 
         System.out.println("p6");
-        for (int i = 0; i < LOOP_PER_PATTERNS; i++) {
+        for (int i = 0; i < patternLoopCount; i++) {
             RandomStringPair p = RandomStringPair.nextRandomPatchedPair(rand);
             bdf.benchmark("base:random <> cmp:(base x random patch)", p);
             System.out.print(".");
         }
         System.out.println("");
 
+        System.out.println("files-diff");
+        try {
+            bdf.benchmarkFiles();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         bdf.printResults();
     }
 
     public static void main(String[] args) {
-        System.out.println("[Benchmark Session1]");
-        doBenchmark();
-        System.out.println("[Benchmark Session2]");
-        doBenchmark();
-        System.out.println("[Benchmark Session3]");
-        doBenchmark();
+        int sessionCount = 3;
+        int patternLoopCount = 5;
+        if (args.length == 0) {
+            System.out.println("args: <session_count> <pattern_loop_count> <files_diff_base_dir> <files_diff_cmp_dir>");
+        }
+        if (args.length > 0) {
+            sessionCount = Integer.parseInt(args[0]);
+        }
+        if (args.length > 1) {
+            patternLoopCount = Integer.parseInt(args[1]);
+        }
+        File filesDiffBaseDir = null;
+        File filesDiffCmpDir = null;
+        if (args.length > 3) {
+            filesDiffBaseDir = new File(args[2]);
+            filesDiffCmpDir = new File(args[3]);
+        }
+        for (int i = 1; i <= sessionCount; i++) {
+            System.out.println("[Benchmark Session" + i + "]");
+            doBenchmark(patternLoopCount, filesDiffBaseDir, filesDiffCmpDir);
+        }
     }
 }
